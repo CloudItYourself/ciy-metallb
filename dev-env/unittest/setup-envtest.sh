bb4b978adc151d7864feae5aca9c39d38ccbf6df
ronen4822 <ronen4822@gmail.com>
2024-03-22 15:39:06 +0200

    ?

--- dev-env/unittest/setup-envtest.sh
+++ dev-env/unittest/setup-envtest.sh
@@ -3,4 +3,4 @@
 bin_dir=$1/bin/
 
 mkdir -p ${bin_dir}
-GOBIN=${bin_dir} go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+GOBIN=${bin_dir} go install sigs.k8s.io/controller-runtime/tools/setup-envtest@v0.0.0-20230216140739-c98506dc3b8e
